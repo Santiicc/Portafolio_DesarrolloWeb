66b222b9a9e990b9ab92420f7a99a4b66e75a5e4
Santiago Caruso <santiago.caruso@correo.ucu.edu.uy>
2024-04-01 17:50:32 -0300

    Faketrelloo

--- UT1TA5/logic.js
+++ UT1TA5/logic.js
@@ -1,36 +1,165 @@
-// Obtener el modal
-var modal = document.getElementById("myModal");
+document.getElementById('Nochebtn').addEventListener('click', function() {
+  document.body.style.backgroundImage = "url('noche.jpg')" ;
+  document.getElementById('style').href = "stylesnight.css"
+});
 
-// Obtener el span que cierra el modal
-var span = document.getElementsByClassName("close")[0];
 
-// Obtener el párrafo dentro del modal para la descripción de la tarea
-var taskDescription = document.getElementById("taskDescription");
 
-// Función para abrir el modal con la descripción de la tarea
-function openModal(description) {
-  taskDescription.textContent = description;
-  modal.style.display = "block";
+document.getElementById('Diabtn').addEventListener('click', function() {
+  document.body.style.backgroundImage = "url('mountains.jpeg')"  ;
+  document.getElementById('style').href = "styles.css"
+});
+
+
+
+
+function agregarTareaToDo(nombretarea) {
+ 
+  var nuevoBoton = document.createElement('button');
+
+  var nuevoParrafo = document.createElement('p');
+  var Tarea = prompt('Ingresar tarea:');
+  nuevoParrafo.textContent = Tarea;
+  nuevoBoton.appendChild(nuevoParrafo);
+
+  var botonEditar = document.createElement('button');
+  botonEditar.innerHTML = '<i class="fa-solid fa-pencil"></i>';
+  botonEditar.onclick = function(){
+    editar(nuevoParrafo)
+  };
+  nuevoBoton.appendChild(botonEditar);
+
+  var botonBorrar = document.createElement('button');
+  botonBorrar.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  botonBorrar.onclick = function(){
+    borrar(nuevoBoton)
+  };
+  nuevoBoton.appendChild(botonBorrar);
+
+  var botonMover = document.createElement('button');
+  botonMover.innerHTML = '<i class="fa-solid fa-right-to-bracket"></i>';
+  botonMover.onclick = function(){
+    mover(nuevoBoton)
+  };
+  nuevoBoton.appendChild(botonMover);
+
+  document.getElementById('TareasToDo').appendChild(nuevoBoton);
 }
 
-// Cuando el usuario haga clic en el span (x), cierra el modal
-span.onclick = function() {
-  modal.style.display = "none";
+
+
+
+
+function agregarTareaDoing(nombretarea) {
+ 
+  var nuevoBoton = document.createElement('button');
+
+  var nuevoParrafo = document.createElement('p');
+  var Tarea = prompt('Ingresar tarea:');
+  nuevoParrafo.textContent = Tarea;
+  nuevoBoton.appendChild(nuevoParrafo);
+
+  var botonEditar = document.createElement('button');
+  botonEditar.innerHTML = '<i class="fa-solid fa-pencil"></i>';
+  botonEditar.onclick = function(){
+    editar(nuevoParrafo)
+  };
+  nuevoBoton.appendChild(botonEditar);
+
+  var botonBorrar = document.createElement('button');
+  botonBorrar.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  botonBorrar.onclick = function(){
+    borrar(nuevoBoton)
+  };
+  nuevoBoton.appendChild(botonBorrar);
+
+  var botonMover = document.createElement('button');
+  botonMover.innerHTML = '<i class="fa-solid fa-right-to-bracket"></i>';
+  botonMover.onclick = function(){
+    mover(nuevoBoton)
+  };
+  nuevoBoton.appendChild(botonMover);
+
+  document.getElementById('TareasDoing').appendChild(nuevoBoton);
 }
 
-// Cuando el usuario haga clic fuera del modal, también se cierra
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+
+
+
+
+
+
+
+function agregarTareaDone() {
+ 
+  var nuevoBoton = document.createElement('button');
+
+  var nuevoParrafo = document.createElement('p');
+  var Tarea = prompt('Ingresar tarea:');
+  nuevoParrafo.textContent = Tarea;
+  nuevoBoton.appendChild(nuevoParrafo);
+
+  var botonEditar = document.createElement('button');
+  botonEditar.innerHTML = '<i class="fa-solid fa-pencil"></i>';
+  botonEditar.onclick = function(){
+    editar(nuevoParrafo)
+  };
+  nuevoBoton.appendChild(botonEditar);
+
+  var botonBorrar = document.createElement('button');
+  botonBorrar.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  botonBorrar.onclick = function(){
+    borrar(nuevoBoton)
+  };
+  nuevoBoton.appendChild(botonBorrar);
+
+  var botonMover = document.createElement('button');
+  botonMover.innerHTML = '<i class="fa-solid fa-right-to-bracket"></i>';
+  botonMover.onclick = function(){
+    mover(nuevoBoton)
+  };
+  nuevoBoton.appendChild(botonMover);
+
+  document.getElementById('TareasDone').appendChild(nuevoBoton);
+}
+
+
+
+
+
+
+
+function mover(boton){
+  var destino = prompt("Ingrese a donde mover la tarea (ToDo, Doing o Done):");
+  if (destino === 'ToDo') {
+    document.getElementById('TareasToDo').appendChild(boton);
+  } else if (destino === 'Doing') {
+    document.getElementById('TareasDoing').appendChild(boton);
+  } else if (destino === 'Done' || destino === 'Donae') {
+    document.getElementById('TareasDone').appendChild(boton);
+  } else {
+    alert("Ubicación de destino no válida.");
   }
 }
 
-// Obtener todas las tareas (párrafos dentro de los divs de Tareas)
-var tasks = document.querySelectorAll(".Tareas p");
 
-// Agregar un controlador de eventos para cada tarea
-tasks.forEach(function(task) {
-  task.addEventListener("click", function() {
-    openModal(task.textContent);
-  });
-});
+
+
+
+
+function editar(parrafo){
+  var nuevoNombre = prompt('Ingrese el nuevo nombre de la tarea:', parrafo.textContent);
+  if (nuevoNombre) {
+    parrafo.textContent = nuevoNombre;
+  }
+}
+
+
+
+
+function borrar(boton){
+  if (confirm('¿Desea borrar la tarea?')) {
+    boton.remove();
+  }
+}
+
